cbb5ed1c54b300bc9626202552ff0d12bbb54e0a
Felix Martinez Casadiego <felix.martinez@simonelectric.com>
2025-05-07 21:51:17 +0200

    chore: add invalidateQueries in useProductMutation

--- src/products/hooks/useProductMutation.tsx
+++ src/products/hooks/useProductMutation.tsx
@@ -1,11 +1,26 @@
-import { useMutation } from "@tanstack/react-query";
-import { producActions } from "..";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { producActions, Product } from "..";
 
 export function useProductMutation() {
+  const queryClient = useQueryClient();
+
   const mutation = useMutation({
     mutationFn: producActions.createProduct,
-    onSuccess: () => console.log("Producto creado"),
-    onSettled: () => console.log("on settled"),
+    onSuccess: (data) => {
+      //   queryClient.invalidateQueries({
+      //     queryKey: ["products", { filterKey: data.category }],
+      //   });
+
+      queryClient.setQueryData<Product[]>(
+        ["products", { filterKey: data.category }],
+        (old) => {
+          const productData = data as Product; // Ensure data is treated as Product
+          if (!old) return [productData];
+
+          return [...old, productData];
+        }
+      );
+    },
   });
 
   return mutation;
